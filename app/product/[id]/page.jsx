2ab7bc93040261c5e3c6abbb245a2913409e6f97
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-03-02 23:37:05 +0300

    create backend search

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import axios from "axios";
 
 const getProductDetails = async (id) => {
-    const { data } = await axios.get(`${process.env.API_URL}/api/products/${id}`);
-    return data?.product
+  const { data } = await axios.get(`${process.env.API_URL}/api/products/${id}`);
+  return data?.product;
+};
 
-}
-
-const ProductDetailsPage = async({ params }) => {
-    const product = await getProductDetails(params.id);
-  //  console.log("PRODUCT=>",product);
+const ProductDetailsPage = async ({ params }) => {
+  const product = await getProductDetails(params.id);
   return <ProductDetails product={product} />;
 };
 
--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,27 +1,29 @@
 import Product from "../models/product";
+import APIFilters from "../utils/APIFilters";
 
 export const newProduct = async (req, res, next) => {
-    const product = await Product.create(req.body);
-    res.status(201).json({
-        product
-    })
-}
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    product,
+  });
+};
 
 export const getProducts = async (req, res, next) => {
-    const products = await Product.find();
-    res.status(200).json({
-        products
-    })
-}
+  const apiFilters = new APIFilters(Product.find(), req.query).search();
+  const products = await apiFilters.query;
+  res.status(200).json({
+    products,
+  });
+};
 
 export const getProduct = async (req, res, next) => {
-    const product = await Product.findById(req.query.id);
-    if (!product) {
-        res.status(404).json({
-            error:"Product not found."
-        })
-    }
-    res.status(200).json({
-        product
-    })
-}
+  const product = await Product.findById(req.query.id);
+  if (!product) {
+    res.status(404).json({
+      error: "Product not found.",
+    });
+  }
+  res.status(200).json({
+    product,
+  });
+};
