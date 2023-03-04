88c5b103d9d1324d3c66061869b873f322ceb4b1
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-03-04 03:55:37 +0300

    add filter functionalty

--- backend/utils/APIFilters.js
+++ backend/utils/APIFilters.js
@@ -12,7 +12,6 @@ class APIFilters {
           },
         }
       : {};
-    console.log(keyword);
     this.query = this.query.find({ ...keyword });
     return this;
   }
@@ -20,7 +19,21 @@ class APIFilters {
     const queryCopy = { ...this.queryStr };
     const removeFields = ["keyword", "page"];
     removeFields.forEach((el) => delete queryCopy[el]);
-    this.query = this.query.find(queryCopy);
+    let output = {};
+    let prop = "";
+    for (let key in queryCopy) {
+      if (!key.match(/\b(gt|gte|lt|lte)/)) {
+        output[key] = queryCopy[key];
+      } else {
+        prop = key.split("[")[0];
+        let operator = key.match(/\[(.*)\]/)[1]
+        if (!output[prop]) {
+          output[prop] = {};
+        }
+        output[prop][`$${operator}`] = queryCopy[key];
+      }
+    }
+    this.query = this.query.find(output);
     return this;
   }
 }
