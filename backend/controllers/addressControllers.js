ab883569cb86d45fd75a3512c231d06bed8806a2
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-04-07 20:48:59 +0300

    add address

--- backend/controllers/addressControllers.js
+++ backend/controllers/addressControllers.js
@@ -1,6 +1,8 @@
 import Address from "../models/address";
 
 export const newAddress = async (req, res) => {
+  req.body.user = req.user._id;
+
   const address = await Address.create(req.body);
 
   res.status(200).json({
@@ -9,9 +11,35 @@ export const newAddress = async (req, res) => {
 };
 
 export const getAddresses = async (req, res) => {
-  const addresses = await Address.find()
+  const addresses = await Address.find({ user: req.user._id });
 
   res.status(200).json({
     addresses,
+  });
+};
+
+export const getAddress = async (req, res) => {
+  const address = await Address.findById(req.query.id);
+
+  if (!address) {
+    return next(new ErrorHandler("Address not found", 404));
+  }
+
+  res.status(200).json({
+    address,
+  });
+};
+
+export const updateAddress = async (req, res) => {
+  let address = await Address.findById(req.query.id);
+
+  if (!address) {
+    return next(new ErrorHandler("Address not found", 404));
+  }
+
+  address = await Address.findByIdAndUpdate(req.query.id, req.body);
+
+  res.status(200).json({
+    address,
   });
 };
